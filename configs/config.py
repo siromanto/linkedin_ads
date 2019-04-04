67472d8ebef374dacecf5580c9dd2b3b2bf53d8e
siromanto <dmytrenko.serhii@gmail.com>
2019-04-04 20:16:43 +0300

    one more env to test into production

--- configs/config.py
+++ configs/config.py
@@ -4,13 +4,19 @@ CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/cr
 DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/SnowflakeKeys/Siromanto_account.json'
 
 
-# Production test env
+# Production dev env
 # DATA_PATH = '../data/linkedin_raw_data.csv'
 # CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/LinkedinAdsKeys/Linkedin_prod_test.json'
 # DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/SnowflakeKeys/SnowflakeComputing.json'
 
 
+# Production airflow test path
+# DATA_PATH = 'data/linkedin_raw_data.csv'
+# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/LinkedinAdsKeys/Linkedin_prod_test.json'
+# DB_CONFIG_PATH = 'credentials/SnowflakeKeys/SnowflakeComputing.json'
+
+
 # Production path
 # DATA_PATH = 'data/linkedin_raw_data.csv'
-# CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/LinkedinAdsKeys/LinkedinAds.json'
-# DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/SnowflakeKeys/SnowflakeComputing.json'
+# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/LinkedinAdsKeys/Linkedin1.json'
+# DB_CONFIG_PATH = 'credentials/SnowflakeKeys/SnowflakeComputing.json'
